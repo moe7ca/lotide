95bc9ad661ab2ce16843dfbe3d395c77724a3cba
Ahmed Noori <moe7ca@gmail.com>
2021-11-29 22:01:13 -0500

    Update const eqArrays.js

--- const eqArrays.js
+++ const eqArrays.js
@@ -15,7 +15,7 @@ const eqArrays = function(arr1, arr2) {
   return true;
 };
 
-assertEqual(eqArrays([1, 2, 3], [1, 2, 3]), true); // => should PASS
+assertEqual(eqArrays([1, 2, 3], [1, 2, 3]), true);
 assertEqual(eqArrays([1, 2, 3], [3, 2, 1]), false);
 assertEqual(eqArrays([1, 2, 3], [3, 2, 1]), true);
 assertEqual(eqArrays([1, 2, 3], [1, 2, 3]), false);
