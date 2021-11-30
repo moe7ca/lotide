4333c1300d8cd0ba1a6f11784eee53d223ca524d
Ahmed Noori <moe7ca@gmail.com>
2021-11-30 02:22:07 -0500

    Exporting All Functions

---
+++ index.js
@@ -0,0 +1,35 @@
+// index.js
+const head   = require('./head');
+const tail   = require('./tail');
+const middle = require('./middle');
+const countLetters = require('./countLetters');
+const assertArraysEqual = require('./assertArraysEqual');
+const assertEqual = require('./assertEqual');
+const countOnly = require('./countOnly');
+const eqArrays = require('./eqArrays');
+const eqObjects = require('./eqObjects');
+const findKey = require('./findKey');
+const findKeyByValue = require('./findKeyByValue');
+const flatten = require('./flatten');
+const map = require('./map');
+const takeUntil = require('./takeUntil');
+const without = require('./without');
+
+
+module.exports = {
+  head,
+  tail,
+  middle,
+  countLetters,
+  assertArraysEqual,
+  assertEqual,
+  countOnly,
+  eqArrays,
+  eqObjects,
+  findKey,
+  findKeyByValue,
+  flatten,
+  map,
+  takeUntil,
+  without
+};
