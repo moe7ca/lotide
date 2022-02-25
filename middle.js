089de30d72bf6c5974fe50e0393a9a677ee5cc1c
Ahmed Noori <moe7ca@gmail.com>
2022-02-25 03:43:31 -0500

    Implement middle

--- middle.js
+++ middle.js
@@ -1,6 +1,6 @@
 const middle = function(array) {
   let midArray = Math.ceil((array.length / 2));
-  //console.log(midArray);
+
   if (array.length <= 2) {
     return [];
   } else if (array.length % 2 !== 0) {
